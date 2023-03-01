db00bbe81b35c0f09da0cf98941e0c9382eb4dae
damora.20465 <damora.20465@LABI2-13PC6>
2023-03-01 13:48:52 +0100

    Aggiungere i file di progetto.

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+/* using System.Collections.Generic;
+using System.Drawing;
+using System.Dynamic;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.ComponentModel;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading; */
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace lista_CRUD
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
